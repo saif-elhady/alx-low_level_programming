8709df8848a39147167d6eb64d6acfe19d973ec3
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-10 10:08:48 -0700

    7th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of	a char: %i byte(s)\n", sizeof(char));
-printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-printf("Size of flaot: %i byte(s)\n", sizeof(float));
+printf("Size of	a char: %ibyte(s)\n", sizeof(char));
+printf("Size of an int: %ibyte(s)\n", sizeof(int));
+printf("Size of a long int: %ibyte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ibyte(s)\n", sizeof(long long int));
+printf("Size of flaot: %ibyte(s)\n", sizeof(float));
 return (0);
 }
