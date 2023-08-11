2d258b6676af53baa303b92cf80590968e0fc091
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-11 01:39:17 -0700

    1st commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else
+	else if(n<0)
 	{
 		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
 	}	
 	return (0);
 }
