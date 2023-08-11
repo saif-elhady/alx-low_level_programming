270c4aa03c7c611ff3f534a2ee4258dabe0a3426
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-11 11:34:41 -0700

    8th commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);	
