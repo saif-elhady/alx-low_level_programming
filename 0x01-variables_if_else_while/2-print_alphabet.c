f35d6aa3aa8ebf1c2cc841260629d321c1e0fc41
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-11 09:07:15 -0700

    3rd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = "a"; c <= "z"; ++c)
+{	
+putchar(c);
+}
+putchar("\n");
+return (0);
+}
