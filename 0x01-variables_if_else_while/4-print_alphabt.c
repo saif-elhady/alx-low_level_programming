0e68b0f312cd875bb18d8fe8ac45f177680b7759
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-11 09:56:34 -0700

    4th commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	for (c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		else
+			putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
