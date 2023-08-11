ee6ec7e9d050e5b09ccb14d2f9916683816a4ccf
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-11 12:08:33 -0700

    9th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 0;
+	char c = 'a';
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+
+	while (c < 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
