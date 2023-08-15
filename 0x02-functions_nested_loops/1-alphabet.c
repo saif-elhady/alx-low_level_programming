15566f3b578315972e32568aca2f82baa52b4d1e
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-15 12:48:19 -0700

    2nd commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
+#include "holberton.h"
+
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
-int main(void)
+void print_alphabet(void)
 {
-
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+		_putchar(letter);
 
-	putchar('\n');
-	return (0);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
-
 void print_alphabet(void);
-#endif
