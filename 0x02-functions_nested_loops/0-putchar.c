24e47d036350e779c343dc52248757abcaa3f7e7
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-15 08:18:36 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char str [] = "_putchar";
+	int i = 0;
+
+	for (i =0; i <= strlen(str); i++)
+	{
+		putchar(str[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
