a158f5537f203a826e54ce4b087174643f3a6301
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-15 10:33:20 -0700

    2nd commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,11 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+void print_alphabet(void);
+#endif
