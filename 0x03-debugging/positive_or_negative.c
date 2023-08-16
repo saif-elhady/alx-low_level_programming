8e94190f1cddc3b8ae0be4d4e79c5d72b135c249
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-16 11:29:55 -0700

    1st commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+void positive_or_negative(int i)
+{
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
