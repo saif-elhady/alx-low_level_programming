b67cd04c1d286a6b04b7357838ae08220e1a0c0a
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-21 15:22:00 -0700

    5th commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
