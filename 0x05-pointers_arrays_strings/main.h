ba7d5986e5fa626974f23db9ac73514f5ef9a700
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-21 08:42:42 -0700

    1st commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
