0d74919462530098ad81b88a5f2f2e7a0e0645a4
fatlaelgamed <saifelhady2004@gmail.com>
2023-08-23 05:43:09 -0700

    1st commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef main_H
+#define main_H
+
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+#endif
