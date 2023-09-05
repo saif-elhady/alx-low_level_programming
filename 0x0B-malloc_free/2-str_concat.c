8919360d0e22913dc04e243760a4797f715c9c23
fatlaelgamed <saifelhady2004@gmail.com>
2023-09-05 08:50:20 -0700

    3rd commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (index = 0; s1[index] || s2[index]; index++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 	s3 =  malloc(sizeof(char) * len);
 	if (s3 == NULL)
