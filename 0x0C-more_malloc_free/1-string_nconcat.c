ae6163dae32e4fbb0a11032ffa31715895aa430d
fatlaelgamed <saifelhady2004@gmail.com>
2023-09-07 17:14:12 -0700

    2nd commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, j = 0;
+	char *s3[];
+	int len = 0;
+
+	while (s1[i] != '\0')
+	{
+		s3[j] = s1[i];
+		i++;
+		j++;
+	}
+
+
+	for (i = 0; i < n; i++)
+	{
+		s3[j] = s2[i];
+		j++;
+	}
+	i =0;
+	while (s3[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	void *s3 =  malloc(sizeof(char) * len + 1);
+
+	if (s3 == NULL)
+		return (NULL);
+
+	s3[len] = '\0';
+	return (s3);
+}
