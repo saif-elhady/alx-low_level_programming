e393d8cd50bca3e207d452b6cd7acb7aad5fd07d
fatlaelgamed <saifelhady2004@gmail.com>
2023-09-07 17:39:05 -0700

    3rd commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
 
-	if (nmem == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr =  malloc(size * nmemb);
 	if (arr == NULL)
